7121d4c5bbef4f29bc42bdae1cc0531c848fadcb
aziza2607 <nouriaziza@hotmail.fr>
2025-05-02 15:43:30 +0200

    Update index.js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -23,6 +23,7 @@ product_price: Number(price)
 })
 })
 }
+
 const cartItemsListeners = () => {
   const addToCartButtons = document.querySelectorAll('.cart .add-to-cart')
   listenToAdd(addToCartButtons)
